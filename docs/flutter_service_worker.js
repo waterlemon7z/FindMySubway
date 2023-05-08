2b3f461d121fb6f20c171a248b0f2f45626f63cc
waterlemon7z <lemon7z@icloud.com>
2023-05-08 11:32:38 +0900

    Fix web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c965dcd32792ad11c9b00cbcf7467e50",
-"index.html": "4da86bb3884a824ff159d997fc8fc221",
-"/": "4da86bb3884a824ff159d997fc8fc221",
+"index.html": "3f3f2b2a05885d98cad2c007e8713058",
+"/": "3f3f2b2a05885d98cad2c007e8713058",
 "main.dart.js": "5830f014de5880909a833523e68fc2c4",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "a91be7f8d5ef88bd0ec195fc12665624",
 "assets/AssetManifest.json": "7b672bd8e50a35423bc3456addd2cc2f",
 "assets/NOTICES": "90af76062dae861fdef45c8769271715",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",

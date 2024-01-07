20325d7b5b51b031656ca166689c376a09d3e05a
waterlemon7z <lemon7z@icloud.com>
2024-01-07 16:17:28 +0900

    [DEL] no more use files

--- android/app/src/main/kotlin/com/example/find_my_subway/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.find_my_subway
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/findMySubway/find_my_subway/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package findMySubway.find_my_subway
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
